103eac308f152a542961a6d53fe11fe5dfc51b83
Stephen Heron <stephen.heron@gmail.com>
2014-12-20 18:51:52 +0000

    Added screenshot task

---
+++ screenshot.casper.js
@@ -0,0 +1,29 @@
+var casper = require('casper').create({
+    //verbose: true,
+    //logLevel: 'debug',
+    onError: function(self, m) { // Any "error" level message will be written
+        console.log('FATAL:' + m); // on the console output and PhantomJS will
+        self.exit(); // terminate
+        process.exit(1);
+    }
+});
+
+if (casper.cli.args.length < 3) {
+    console.log('Usage: screenshot.casper.js <some URL> <width> <height>');
+    process.exit(1);
+} else {
+    var address = casper.cli.args[0];
+    var viewport = {
+        width: casper.cli.args[1],
+        height: casper.cli.args[2]
+    };
+}
+
+
+casper.start(address, function(){
+    casper.viewport(viewport.width, viewport.height, function(){
+        console.log(this.captureBase64('png'));
+    });
+});
+
+casper.run();
